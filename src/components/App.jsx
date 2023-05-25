f555af922c533840b6eebdce6dd4b4d335f4a08c
barkode <konstantinks@gmail.com>
2023-05-25 22:45:15 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
+import React, { Component } from 'react';
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101',
+    // }}
     >
-      React homework template
+      <p>Please leave feedback</p>
+      <p>
+        <button type="button">Good</button>
+      </p>
+      <p>
+        <button type="button">Neutral</button>
+      </p>
+      <p>
+        <button type="button">Bad</button>
+      </p>
+      <p>Statistics:</p>
+      <ul>
+        <li>Good: 0</li>
+        <li>Neutral: 0</li>
+        <li>Bad: 0</li>
+      </ul>
     </div>
   );
 };
