0cfe914e6bc4b265ad8f3154e35e0409cca3649f
barkode <konstantinks@gmail.com>
2023-05-26 01:27:24 +0100

    add functions
    add onClick to buttons. Add increment to buttons.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,74 @@
 import React, { Component } from 'react';
+import './App.module.css';
 
-export const App = () => {
-  return (
-    <div
-    // style={{
-    //   height: '100vh',
-    //   display: 'flex',
-    //   justifyContent: 'center',
-    //   alignItems: 'center',
-    //   fontSize: 40,
-    //   color: '#010101',
-    // }}
-    >
-      <p>Please leave feedback</p>
-      <p>
-        <button type="button">Good</button>
-      </p>
-      <p>
-        <button type="button">Neutral</button>
-      </p>
-      <p>
-        <button type="button">Bad</button>
-      </p>
-      <p>Statistics:</p>
-      <ul>
-        <li>Good: 0</li>
-        <li>Neutral: 0</li>
-        <li>Bad: 0</li>
-      </ul>
-    </div>
-  );
-};
+class App extends Component {
+  static defaultsProps = {};
+
+  static propTypes = {};
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedbackGood = () => {
+    this.setState(prevState => {
+      return { good: prevState.good + 1 };
+    });
+  };
+  handleFeedbackNeutral = () => {
+    this.setState(prevState => {
+      return {
+        neutral: prevState.neutral + 1,
+      };
+    });
+  };
+  handleFeedbackBad = () => {
+    this.setState(prevState => {
+      return {
+        bad: prevState.bad + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {};
+  countPositiveFeedbackPercentage = () => {};
+  countNeutralFeedbackPercentage = () => {};
+  countBadFeedbackPercentage = () => {};
+
+  render() {
+    return (
+      <div>
+        <p>Please leave feedback</p>
+        <p>
+          <button type="button" onClick={this.handleFeedbackGood}>
+            Good
+          </button>
+        </p>
+        <p>
+          <button type="button" onClick={this.handleFeedbackNeutral}>
+            Neutral
+          </button>
+        </p>
+        <p>
+          <button type="button" onClick={this.handleFeedbackBad}>
+            Bad
+          </button>
+        </p>
+        <p>Statistics:</p>
+        <ul>
+          <li>Good: {this.state.good}</li>
+          <li>Neutral: {this.state.neutral}</li>
+          <li>Bad: {this.state.bad}</li>
+          <li>Total: 000</li>
+          <li>Positive feedback: 0%</li>
+          <li>Neutral feedback: 0%</li>
+          <li>Bad feedback: 0%</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
