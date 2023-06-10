4d7d006363df83fdbc2127e4049421225b867698
barkode <konstantinks@gmail.com>
2023-06-10 02:02:11 +0100

    add feedback

--- src/components/Controls/FeedbackOptions.jsx
+++ src/components/Controls/FeedbackOptions.jsx
@@ -1,10 +1,26 @@
 import { Component } from 'react';
+import propTypes from 'prop-types';
 
 class FeedBackOptions extends Component {
+  static propTypes = {
+    options: propTypes.arrayOf(propTypes.string.isRequired).isRequired,
+    onLeaveFeedback: propTypes.func.isRequired,
+  };
   render() {
-    const { good, neutral, bad } = this.props;
-    return;
-    <></>;
+    const { options, onLeaveFeedback } = this.props;
+    return (
+      <div>
+        {options.map(option => (
+          <button
+            type="button"
+            key={option}
+            onClick={() => onLeaveFeedback(option)}
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+    );
   }
 }
 
